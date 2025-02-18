fc7a6ba62b28dd121f939abc1f6117379ac9a0e6
Yaroslav Mykhalchuk <ipzm222_myaa@student.ztu.edu.ua>
2025-02-19 01:50:19 +0200

    fix sybbolic link

--- config/filesystems.php
+++ config/filesystems.php
@@ -71,7 +71,7 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
-        base_path('www') => public_path('public'),
+        base_path('www') => public_path(),
     ],
 
 ];
